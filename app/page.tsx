270270367ba53d41c147e10d76a1bff92e7e51cd
Bartek <bartekmackowiak92@gmail.com>
2024-11-26 18:26:12 +0100

    eslint and prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl  font-black text-white">
+    <h1 className="text-3xl   font-black text-white">
       Welcome to the word with next.js
     </h1>
   );
