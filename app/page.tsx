1e55218d497f0c7c9a6c23183d8b5988b42e5ad3
Bartek <bartekmackowiak92@gmail.com>
2024-11-28 20:15:51 +0100

    tailwinnd css theme config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl   font-black text-white">
+    <div className="mx-auto mt-10 max-w-sm rounded-lg border border-gray-300 bg-gray-100 p-6 text-center text-lg font-semibold leading-relaxed tracking-wide text-gray-700 shadow-lg">
       Welcome to the word with next.js
-    </h1>
+    </div>
   );
 }
