6e6da893b1da21c87b97092e4fdfe3e7eb469a7a
Bartek <bartekmackowiak92@gmail.com>
2024-11-28 20:38:41 +0100

    assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="mx-auto mt-10 max-w-sm rounded-lg border border-gray-300 bg-gray-100 p-6 text-center text-lg font-semibold leading-relaxed tracking-wide text-gray-700 shadow-lg">
-      Welcome to the word with next.js
-    </div>
+    <>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+    </>
   );
 }
