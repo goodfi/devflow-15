f4a635b792b979a0b49e1334200c95fc973789ec
Bartek <bartekmackowiak92@gmail.com>
2024-12-04 22:41:09 +0100

    auth with github

--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -1,14 +1,41 @@
+"use client";
+
 import Image from "next/image";
+import { signIn } from "next-auth/react";
 import React from "react";
+
+import ROUTES from "@/constants/route";
+import { toast } from "@/hooks/use-toast";
 
 import { Button } from "../ui/button";
 
 const SocialAuthForm = () => {
   const ButtonClass =
     "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5 ";
+
+  const handleSignIn = async (provider: "github" | "google") => {
+    try {
+      await signIn(provider, {
+        callbackUrl: ROUTES.HOME,
+        redirect: false,
+      });
+    } catch (error) {
+      console.log(error);
+
+      toast({
+        title: "Sign-in failed",
+        description:
+          error instanceof Error
+            ? error.message
+            : "An error occured during sign-in",
+        variant: "destructive",
+      });
+    }
+  };
+
   return (
     <div className="mt-10 flex flex-wrap gap-2.5">
-      <Button className={ButtonClass}>
+      <Button className={ButtonClass} onClick={() => handleSignIn("github")}>
         <Image
           src="/icons/github.svg"
           alt="github logo"
@@ -18,7 +45,7 @@ const SocialAuthForm = () => {
         />
         <span>Log in with Github</span>
       </Button>
-      <Button className={ButtonClass}>
+      <Button className={ButtonClass} onClick={() => handleSignIn("google")}>
         <Image
           src="/icons/google.svg"
           alt="google logo"
