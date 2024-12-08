4811df82feac65f6f7e46d15f074b4a988e4d2ae
Bartek <bartekmackowiak92@gmail.com>
2024-12-08 22:21:04 +0100

    more update

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -16,14 +16,14 @@ const QuestionCard = ({
 }: Props) => {
   return (
     <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
-      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
+      <div className="flex w-full flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div>
           <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
             {getTimeStamp(createdAt)}
           </span>
 
           <Link href={ROUTES.QUESTION(_id)}>
-            <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
+            <h3 className="sm:h3-semibold base-semibold  text-dark200_light900 line-clamp-1 flex-1 justify-center">
               {title}
             </h3>
           </Link>
