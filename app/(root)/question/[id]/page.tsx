f27748149c1c39a134a80f5ec3d9b3aad86f2edd
Bartek <bartekmackowiak92@gmail.com>
2025-03-31 17:35:05 +0200

    implement vodet ui

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -8,6 +8,7 @@ import { Preview } from "@/components/editor/Preview";
 import AnswerForm from "@/components/forms/Answerform";
 import Metric from "@/components/Metric";
 import UserAvatar from "@/components/UserAvatar";
+import Votes from "@/components/votes/Votes";
 import ROUTES from "@/constants/route";
 import { getAnswers } from "@/lib/actions/answer.action";
 import { getQuestion, incrementViews } from "@/lib/actions/question.action";
@@ -54,7 +55,12 @@ const QuestionDetails = async ({ params }: RoutParams) => {
           </div>
 
           <div className="flex justify-end">
-            <p>Votes</p>
+            <Votes
+              upvotes={question.upvotes}
+              hasupVoted={true}
+              downvotes={question.downvotes}
+              hasdownVoted={false}
+            />
           </div>
         </div>
 
